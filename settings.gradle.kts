88f8d22b47240517ab37f10ddcad48478ff85834
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2025-05-16 12:11:03 +0200

    MCP-14 Implement Gradle caching for the build (#13)

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "sonar-mcp-server"
+
+plugins {
+    id("com.gradle.develocity") version "3.18.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.2.1"
+}
+
+val isCiServer = System.getenv("CI") != null
+
+buildCache {
+    local {
+        isEnabled = !isCiServer
+    }
+    remote(develocity.buildCache) {
+        isEnabled = true
+        isPush = isCiServer
+    }
+}
+
+develocity {
+    server = "https://develocity.sonar.build"
+    buildScan {
+        publishing.onlyIf { false }
+    }
+}
